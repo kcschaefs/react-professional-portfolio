b1039cc2ddfae5cb0a6df861fae4e85b6eff96da
Kristen Schaefer <kristencschaefer@gmail.com>
2023-03-08 14:34:16 -0600

    initial commit, file structure, and start of working code

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header"
+import Footer from "./Footer"
+import About from "./About"
+
+
+export { default as Header } from "./Header"
+export { default as Footer } from "./Footer"
+export { default as About } from "./About"
