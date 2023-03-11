41bd7aed79a47e3ca0b02bb2d64dc463b5dbc619
Kristen Schaefer <kristencschaefer@gmail.com>
2023-03-11 14:10:05 -0600

    final bug fixes

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import '../assets/css/About.css'
 import MyResume from './Tech-Resume-Kristen-Schaefer.pdf'
-import { Document, Page } from 'react-pdf'
 import SinglePagePDFViewer from "./pdf/single-page";
 import '../assets/css/Resume.css';
 
@@ -11,14 +10,15 @@ function Resume() {
   return (
     <Container fluid>
       <div className="resumeSection">
-      <div>
-        <a href='./Tech-Resume-Kristen-Schaefer.pdf' download>
-          <button><img className="downloadIcon" src="./images/download.png" />My Resume</button></a>
+        <div>
+          <a href={MyResume} download>
+            <button><img className="downloadIcon" src="./images/download.png" />My Resume</button>
+          </a>
         </div>
         <div className="resumeEl">
-            <SinglePagePDFViewer pdf={MyResume} />
+          <SinglePagePDFViewer pdf={MyResume} />
         </div>
-        
+
       </div>
     </Container>
   );
